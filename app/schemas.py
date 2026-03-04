b77a75ba55d8e7c3526bb824c9c110339ded8904
GiselleGarcia25 <giselle.garcia20051@gmail.com>
2026-03-03 23:57:54 -0600

    POST/payments - Endpoints
    - Creé el endpoint POST /payments para registrar pagos y agregué la validación para comprobar que el rental_id pertenezca al cliente.
    - Definí el schema para la creación de pagos y dejé rental_id como opcional.

--- app/schemas.py
+++ app/schemas.py
@@ -8,3 +8,10 @@ class RespuestaDevolucion(BaseModel):
 
     class Config:
         from_attributes = True
+
+class CreacionPagos(BaseModel):
+    customer_id: int
+    staff_id: int
+    amount: float
+    #Giselle: El rental_id es opcional, ya que el cliente puede pagar sin una renta específica
+    rental_id: Optional[int] = None 
